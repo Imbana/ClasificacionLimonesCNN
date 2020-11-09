79469b36ddd01ac2f76dc15c8e9d955ac4652d34
jeison imbana <69469940+Imbana@users.noreply.github.com>
2020-11-08 21:05:48 -0500

    Add files via upload

---
+++ tfliteDemoVideo.py
@@ -0,0 +1,115 @@
+import numpy as np
+import cv2
+
+import matplotlib.pylab as plt
+import argparse
+import io
+import os
+import sys
+import time
+
+import picamera
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
+
+# devuelve un diccionario con todas las clases
+def load_labels(path):
+  with open(path, 'r') as f:
+    return {i: line.strip() for i, line in enumerate(f.readlines())}
+
+
+def clasificar_imagen(interpreter,imagen,top_k=1):
+    
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+    input_shape = input_details[0]['shape']
+    input_data = np.array(imagen, dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    
+    interpreter.invoke()
+
+    output = np.squeeze(interpreter.get_tensor(output_details[0]['index']))
+    
+    ordered = np.argpartition(-output, top_k)
+    return [(i, output[i]) for i in ordered[:top_k]]
+    
+
+def main():
+    
+    # de aqui para asignar la ruta del modelo desde la terminal
+    parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+      '--model', help='File path of .tflite file.', required=True)
+    parser.add_argument(
+      '--labels', help='File path of labels file.', required=True)
+    args = parser.parse_args()
+    
+    
+    #pathLabels=('/home/pi/Documents/labels.txt')    
+    #pathTflite=('/home/pi/Documents/RaspberryLite/Proyecto Final/Classifier_best_val_accuracy.tflite')
+    
+    labels = load_labels(args.labels)
+    
+        
+    interpreter = Interpreter(args.model)
+    interpreter.allocate_tensors()
+    
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 30
+    rawCapture = PiRGBArray(camera, size=(640, 480))   
+    
+    
+    captura = cv2.VideoCapture('/home/pi/Documents/WhatsApp Video 2020-10-26 at 8.40.23 AM.mp4')
+
+    while (captura.isOpened()):
+        
+        ret, imagen = captura.read()
+
+        if ret == True:
+        
+
+            frame=cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+        
+        
+    
+            Imagen_normed = frame/ 255
+    
+        
+            Imagen_show = cv2.resize(imagen, (600, 600))
+    
+            Imagen_resized = cv2.resize(Imagen_normed, (224, 224))
+            Imagen_espnaded=np.expand_dims(Imagen_resized, axis=0)
+    
+          
+            start_time = time.time()
+    
+            results = clasificar_imagen(interpreter, Imagen_espnaded)
+
+            elapsed_ms = round((time.time() - start_time) * 1000,2)
+
+            label_id, prob = results[0]
+    
+            print(labels[label_id]+"   "+str(elapsed_ms))       
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            color=(14,129,60)
+            images = cv2.putText(Imagen_show,str(elapsed_ms)+"ms  "+labels[label_id]+"   "+str(prob) , (00,100), font, 1,color,2, cv2.LINE_AA) 
+               
+
+            cv2.imshow("Frame", images)
+            
+     
+            # salir si se preciona q 
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        
+        cv2.destroyAllWindows()
+    
+if __name__ == '__main__':
+  main()
---
+++ tfliteImagenes.py
@@ -0,0 +1,119 @@
+import numpy as np
+import cv2
+
+import matplotlib.pylab as plt
+import argparse
+import io
+import os
+import sys
+import time
+
+import picamera
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
+
+# devuelve un diccionario con todas las clases
+def load_labels(path):
+  with open(path, 'r') as f:
+    return {i: line.strip() for i, line in enumerate(f.readlines())}
+def clasificar_imagen(interpreter,imagen,top_k=1):
+    
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+    input_shape = input_details[0]['shape']
+    input_data = np.array(imagen, dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    
+    interpreter.invoke()
+
+
+    output = np.squeeze(interpreter.get_tensor(output_details[0]['index']))
+    
+    ordered = np.argpartition(-output, top_k)
+    return [(i, output[i]) for i in ordered[:top_k]]
+    
+
+def main():
+
+    # de aqui para asignar la ruta del modelo desde la terminal
+    parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+      '--model', help='File path of .tflite file.', required=True)
+    parser.add_argument(
+      '--labels', help='File path of labels file.', required=True)
+    
+    args = parser.parse_args()
+    
+    
+    
+    labels = load_labels(args.labels)
+    
+        
+    interpreter = Interpreter(args.model)
+    interpreter.allocate_tensors()
+    
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 30
+    rawCapture = PiRGBArray(camera, size=(640, 480))   
+    
+    pathFolder=('/home/pi/Documents/RaspberryLite/Acaros')
+    contenidoFotos=os.listdir(pathFolder)
+        
+    #stream = io.BytesIO()
+    
+    #for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        
+    #    image = frame.array 
+    for pathFoto in contenidoFotos:
+        
+        nuevaImg=os.path.join(pathFolder,pathFoto)
+        
+        frame1=cv2.imread(nuevaImg)
+        frame=cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+        
+        
+    
+        Imagen_normed = frame/ 255
+    
+        #imagen.shape=(1,224,224,3)
+        Imagen_show = cv2.resize(frame1, (600, 600))
+    
+        Imagen_resized = cv2.resize(Imagen_normed, (224, 224))
+        Imagen_espnaded=np.expand_dims(Imagen_resized, axis=0)
+    
+          
+        start_time = time.time()
+    
+        results = clasificar_imagen(interpreter, Imagen_espnaded)
+
+        elapsed_ms = round((time.time() - start_time) * 1000,2)
+
+        label_id, prob = results[0]
+    
+        print(labels[label_id]+"   "+str(elapsed_ms))       
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        color=(14,129,60)
+        images = cv2.putText(Imagen_show,str(elapsed_ms)+"ms  "+labels[label_id]+"   "+str(prob) , (00,100), font, 1,color,2, cv2.LINE_AA) 
+               
+
+        cv2.imshow("Frame", images)
+        #cv2.waitKey(0)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+     
+
+        
+        
+    cv2.destroyAllWindows()
+ 
+    
+if __name__ == '__main__':
+  main()
